a67bc4f6c843c822eba655a9781b3f1b7d1f635d
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-08 16:28:38 +0300

    responsive layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,18 @@ const Home: NextPage = () => {
         Hello
       </div>
       <div className="p-2"></div>
-      <div className="flex flex-row">
-        <div className="flex w-1/3 flex-col justify-between rounded border-2 border-dashed border-gray-200">
-          <div className="h-1/2 bg-blue-400 rounded border-2 border-dashed border-gray-200 p-8 text-center">
+      <div className="flex flex-col lg:flex-row">
+        <div className="flex w-full lg:w-96 lg:flex-col flex-row justify-between">
+          <div className="h-1/2 bg-blue-400 w-full rounded border-2 border-dashed border-gray-200 p-8 text-center">
             Hello
           </div>
           <div className="p-2"></div>
-          <div className="h-1/2 bg-green-400 rounded border-2 border-dashed border-gray-200 p-8 text-center">
+          <div className="h-1/2 bg-green-400 w-full rounded border-2 border-dashed border-gray-200 p-8 text-center">
             Hello
           </div>
         </div>
         <div className="p-2"></div>
-        <div className=" h-96 bg-red-400 w-2/3 border p-8">Hiya</div>
+        <div className=" h-96 bg-red-400 w-full border p-8">Hiya</div>
       </div>
     </div>
   );
