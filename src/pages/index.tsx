9f4caae8abfcbea3843e5d042c924121042e8161
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-09 09:26:16 +0300

    added trpc route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,28 @@
 import { type NextPage } from "next";
+import { api } from "~/utils/api";
+
 
 const Home: NextPage = () => {
+  const {data, isLoading} = api.example.hello.useQuery({text: 'client'})
   return (
-    <div className="mx-auto flex max-w-screen-xl flex-col py-4">
+    <div className="mx-auto flex max-w-screen-xl flex-col py-2">
       <div className="w-30 rounded border-2 border-dashed border-gray-200  p-4">
-        <InfoCard/>
+        <InfoCard />
       </div>
       <div className="p-2"></div>
       <div className="flex flex-col lg:flex-row">
-        <div className="flex w-full flex-row justify-between lg:w-96 lg:flex-col">
-          <div className="h-1/2 w-full rounded border-2 border-dashed border-gray-200  p-8 text-center">
-            Hello
+        <div className="flex w-full flex-col justify-between sm:flex-row lg:w-96 lg:flex-col">
+          <div className="h-1/2 w-full rounded border-2 border-dashed border-gray-200  p-6 text-center">
+            <InfoCard />
           </div>
           <div className="p-2"></div>
-          <div className="h-1/2 w-full rounded border-2 border-dashed border-gray-200  p-8 text-center">
-            Hello
+          <div className="h-1/2 w-full rounded border-2 border-dashed border-gray-200  p-6 text-center">
+            <InfoCard />
           </div>
         </div>
         <div className="p-2"></div>
-        <div className=" h-96 w-full rounded border-2 border-dashed border-gray-200  p-8">
-          Hiya
+        <div className="w-full rounded border-2 border-dashed border-gray-200  p-6">
+          <InfoCard />
         </div>
       </div>
     </div>
@@ -32,7 +35,7 @@ const InfoCard = () => {
       <div className="px-6 py-4">
         <div className="mb-2 text-xl font-bold">The Coldest Sunset</div>
         <p className="text-base text-gray-700">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibusasl  lsjdlkasdlaksjdlkajldssjalskdjls jdlkasdlaksjdlkajldssjalskdjls jdlkasdlaksjdlkajldssjalskdjls jdlkasdlaksjdlkajldssjalskdjls
         </p>
       </div>
     </div>
