7edccbfe1ac782ccf75eb3bf3d3ff50c0b59ba18
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-09 14:48:06 +0300

    start of card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,34 @@
 import { type NextPage } from "next";
 import { api } from "~/utils/api";
-
+import type { WeatherResponse } from "../server/api/routers/service";
 
 const Home: NextPage = () => {
-  const {data, isLoading} = api.service.weatherapi.useQuery(undefined, {refetchInterval:0, refetchOnReconnect: false, refetchOnWindowFocus: false});
+  const { data, isLoading } = api.service.weatherapi.useQuery(undefined, {
+    refetchInterval: 0,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  });
+  if (!data || !data.limitedResponse) return null;
   return (
     <div className="mx-auto flex max-w-screen-xl flex-col py-2">
       <div className="w-30 rounded border-2 border-dashed border-gray-200  p-4">
-        {isLoading ? "Loading..." : data?.limitedResponse?.daily.temperature_2m_min}
-        {isLoading ? "Loading..." : data?.limitedResponse?.current_weather.temperature}
+        {isLoading
+          ? "Loading..."
+          : data?.limitedResponse?.daily.temperature_2m_min}
+        {isLoading
+          ? "Loading..."
+          : data?.limitedResponse?.current_weather.temperature}
         <InfoCard />
       </div>
       <div className="p-2"></div>
       <div className="flex flex-col lg:flex-row">
         <div className="flex w-full flex-col justify-between sm:flex-row lg:w-96 lg:flex-col">
-          <div className="h-1/2 w-full rounded border-2 border-dashed border-gray-200  p-6 text-center">
+          <div className="w-full rounded border-2 border-dashed border-gray-200  p-6 text-center">
             <InfoCard />
           </div>
           <div className="p-2"></div>
-          <div className="h-1/2 w-full rounded border-2 border-dashed border-gray-200  p-6 text-center">
-            <InfoCard />
+          <div className="w-full rounded border-2 border-dashed border-gray-200 p-6">
+            <BottomCard {...data.limitedResponse} />
           </div>
         </div>
         <div className="p-2"></div>
@@ -37,9 +46,35 @@ const InfoCard = () => {
       <div className="px-6 py-4">
         <div className="mb-2 text-xl font-bold">The Coldest Sunset</div>
         <p className="text-base text-gray-700">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibusasl  lsjdlkasdlaksjdlkajldssjalskdjls jdlkasdlaksjdlkajldssjalskdjls jdlkasdlaksjdlkajldssjalskdjls jdlkasdlaksjdlkajldssjalskdjls
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Voluptatibusasl lsjdlkasdlaksjdlkajldssjalskdjls
         </p>
       </div>
+    </div>
+  );
+};
+
+const BottomCard = (props: WeatherResponse) => {
+  return (
+    <div className="overflow-hidden rounded shadow-lg">
+      <div className="flex border p-4"> hey</div>
+      <div className="flex border p-4"> hey</div>
+      <div className="flex flex-row justify-between border p-4">
+        <div className="flex flex-row">
+          <span>icon</span>
+          <div className="flex flex-col">
+            <b>minimum</b> <span>{props.daily.temperature_2m_min}</span>
+          </div>
+        </div>
+
+        <div className="flex flex-row">
+          <span>icon</span>
+          <div className="flex flex-col">
+            <b>maximum</b> <span>{props.daily.temperature_2m_max}</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex border p-4"> hey </div>
     </div>
   );
 };
--- src/server/api/routers/service.ts
+++ src/server/api/routers/service.ts
@@ -3,8 +3,8 @@ import axios, { AxiosResponse } from "axios";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-interface WeatherResponse {
-  current_weather:{
+export interface WeatherResponse {
+  current_weather: {
     temperature: number;
   };
   daily: {
@@ -19,27 +19,47 @@ interface WeatherResponse {
 
 export const serviceRouter = createTRPCRouter({
   weatherapi: publicProcedure.query(async () => {
-    const f: AxiosResponse<WeatherResponse> = await axios.get("https://api.open-meteo.com/v1/forecast/", {
-      params: {
-        latitude: "52.52",
-        longitude: "13.41",
-        daily:
-          "weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_probability_max",
-        current_weather: true,
-        forecast_days: 1,
-        timezone: "Europe/London",
-      },
-    });
+    const f: AxiosResponse<WeatherResponse> = await axios.get(
+      "https://api.open-meteo.com/v1/forecast/",
+      {
+        params: {
+          latitude: "52.52",
+          longitude: "13.41",
+          daily:
+            "weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_probability_max",
+          current_weather: true,
+          forecast_days: 1,
+          timezone: "Europe/London",
+        },
+      }
+    );
     if (f.status !== 200) {
-      throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Weather API is not working"})
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Weather API is not working",
+      });
     }
     if (!f.data) {
-      throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Weather API is not working"})
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Weather API is not working",
+      });
     }
     const response: WeatherResponse = f.data;
     //validate response
-    if(!response.daily.weathercode || !response.daily.temperature_2m_max || !response.daily.temperature_2m_min || !response.daily.sunrise || !response.daily.sunset || !response.daily.precipitation_probability_max || !response.current_weather.temperature) {
-      throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Weather returned wrong thing?!?!"})
+    if (
+      !response.daily.weathercode ||
+      !response.daily.temperature_2m_max ||
+      !response.daily.temperature_2m_min ||
+      !response.daily.sunrise ||
+      !response.daily.sunset ||
+      !response.daily.precipitation_probability_max ||
+      !response.current_weather.temperature
+    ) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Weather returned wrong thing?!?!",
+      });
     }
 
     const limitedResponse = {
@@ -52,10 +72,11 @@ export const serviceRouter = createTRPCRouter({
         temperature_2m_min: response.daily.temperature_2m_min,
         sunrise: response.daily.sunrise,
         sunset: response.daily.sunset,
-        precipitation_probability_max: response.daily.precipitation_probability_max,
+        precipitation_probability_max:
+          response.daily.precipitation_probability_max,
       },
-    }
-    
+    };
+
     return {
       limitedResponse,
     };
