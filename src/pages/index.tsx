07195be125b4d6cb4d62b313ccd99434c38fb0dd
Larasify <lara.cikis@gmail.com>
2023-06-09 18:26:17 +0300

    daytime

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { type NextPage } from "next";
 import { api } from "~/utils/api";
 import type { WeatherResponse } from "../server/api/routers/service";
-import { Icon } from '@iconify/react';
-
+import { Icon } from "@iconify/react";
+import dayjs from "dayjs";
 
 const Home: NextPage = () => {
   const { data, isLoading } = api.service.weatherapi.useQuery(undefined, {
@@ -56,6 +56,8 @@ const InfoCard = () => {
   );
 };
 
+const titlestyle = "text-sm text-gray-700";
+const valuestyle = "text-sm text-gray-400";
 const BottomCard = (props: WeatherResponse) => {
   return (
     <div className="overflow-hidden rounded shadow-lg">
@@ -63,20 +65,44 @@ const BottomCard = (props: WeatherResponse) => {
       <div className="flex border p-4"> hey</div>
       <div className="flex flex-row justify-between border p-4">
         <div className="flex items-center">
-        <Icon icon="wi:thermometer-exterior" className="text-blue-500 text-3xl" />
+          <Icon
+            icon="wi:thermometer-exterior"
+            className="text-3xl text-blue-500"
+          />
           <div className="flex flex-col">
-            <b>minimum</b> <span>{props.daily.temperature_2m_min}</span>
+            <b className={titlestyle}>Minimum</b> <span className={valuestyle}>{props.daily.temperature_2m_min}</span>
           </div>
         </div>
 
         <div className="flex items-center">
-          <Icon icon="wi:thermometer" className="text-red-500 text-3xl" />
+          <Icon icon="wi:thermometer" className="text-3xl text-red-500" />
           <div className="flex flex-col">
-            <b>maximum</b> <span>{props.daily.temperature_2m_max}</span>
+            <b className={titlestyle}>Maximum</b> <span className={valuestyle}>{props.daily.temperature_2m_max}</span>
           </div>
         </div>
       </div>
-      <div className="flex border p-4"> hey </div>
+
+      <div className="flex flex-row justify-between border p-4">
+        <div className="flex items-center">
+          <Icon icon="wi:horizon-alt" className="text-3xl text-yellow-500" />
+          <div className="flex flex-col ">
+            <b className={titlestyle}>Sunrise</b>{" "}
+            <span className={valuestyle}>
+              {dayjs(props.daily.sunrise).format("HH:MM")}
+            </span>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <Icon icon="wi:horizon" className="text-3xl text-purple-800" />
+          <div className="flex flex-col">
+            <b className={titlestyle}>Sunset</b>{" "}
+            <span className={valuestyle}>
+              {dayjs(props.daily.sunset).format("HH:MM")}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
